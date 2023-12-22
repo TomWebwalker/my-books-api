e946f2137c66e97fe4c9c8cb30ea3f679d7e24dd
Tomasz Flis <tomaszwwl@gmail.com>
2023-12-22 20:53:12 +0100

    db sync

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### Commands
 
+- `gcloud sql connect [DB_NAME] --user=[DB_USER] --quiet`
 - `npm run typeorm schema:sync -- -d data-source.ts` - Sync db (inside container)
--- data-source.ts
+++ data-source.ts
@@ -2,12 +2,6 @@ import { DataSource } from 'typeorm';
 import * as process from 'process';
 
 const AppDataSource = new DataSource({
-  // type: 'mysql',
-  // host: process.env.DB_HOST,
-  // port: +process.env.DB_PORT,
-  // username: process.env.DB_USER,
-  // password: process.env.DB_PASSWORD,
-  // database: process.env.DB_NAME,
   type: 'mysql',
   host: process.env.DB_HOST,
   port: +process.env.DB_PORT,
